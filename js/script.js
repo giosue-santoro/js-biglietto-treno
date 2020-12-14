3f4707e45086c0d8563352b0b7c1cc1204c74526
Giosuè Santoro <giosuesantoro@hotmail.it>
2020-12-14 17:50:30 +0100

    esercizio sistemato

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ var age = prompt('Quanti anni hai?');
 var price = distance * 0.21;
 
 if (age <= 18) {
-  discount = price * 0.2; //Se minorenni 20% di sconto;
+  discount = (price / 100) * 20; //Se minorenni 20% di sconto;
 } else if (age >= 65) {
-  discount = price * 0.4; //Se over 65 40% di sconto;
+  discount = (price / 100) * 40; //Se over 65 40% di sconto;
 }
 
-var output = document.getElementById('result').innerHTML = 'Il prezzo è: ' + discount + '€';
+var finalPrice = price - discount;
+
+var output = document.getElementById('result').innerHTML = 'Il prezzo è: ' + finalPrice + '€';
